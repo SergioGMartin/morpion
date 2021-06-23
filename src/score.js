5d78f68c2b6e3e5feb7236b500f3f70ef8bb1315
Sergio-Martin <38584889+SergioGMartin@users.noreply.github.com>
2021-06-23 18:25:52 +0200

    Update score.js

--- src/score.js
+++ src/score.js
@@ -3,21 +3,6 @@ let scoreCpu;
 
 const initScore = () => {
     
-    const spinner = $(`<div class="spinner-border text-success" role="status">
-    <span class="sr-only"></span></div>`);
-    $("#spin").append(spinner);
-
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET","https://api.jsonbin.io/b/60d32b9b5ed58625fd177079");
-    xhr.setRequestHeader("secret-key","$2b$10$DrQLcmDPnd4qPExl/dCx8ea5xJ0b/IQDMinWHnIihmW7NNi9IPbOy");
-    xhr.onload = () => {
-        let data = JSON.parse(xhr.response);
-        spinner.remove();
-        score.incrementPlayer(data.scorePlayer);
-        score.incrementCpu(data.scoreCPU);
-    };
-    xhr.send();
-
     scorePlayer = $("#scorePlayer");
     scoreCpu = $("#scoreCPU");
 

3534f69f458d605727f74b2e63b8369026fa0f31
SergioGMartin <segamarte@gmail.com>
2021-06-23 18:45:30 +0200

    Initialize

--- src/timer.js
+++ src/timer.js
@@ -1,30 +1,51 @@
-const timer = document.querySelector("#timerTime");
-const date = new Date(0);
+let btnStart;
+let btnPause;
+let btnResume;
+let timer;
+let myCounter;
+let date;
 
-const counter = () => {
-    timer.innerHTML = date.toISOString().substr(14, 5);
-    date.setSeconds(date.getSeconds() + 1);
+const initTimer = () => {
+
+    btnStart = $("#timerStart");
+    btnPause = $("#timerPause");
+    btnResume = $("#timerResume");
+    timer= $("#timerTime");
+    date= new Date(0);
+
+    const reloj = {};
+
+    reloj.counter = () => {
+        timer.text(date.toISOString().substr(14, 5));
+        date.setSeconds(date.getSeconds() + 1);
+    }
+
+    reloj.toggle = (disapear, appear) => {
+        disapear.removeClass("d-block");
+        disapear.addClass("d-none");
+        appear.removeClass("d-none");
+        appear.addClass("d-block");
+    };
+
+    reloj.btnSt = () => {
+        date.setTime(0);
+        reloj.counter();
+        reloj.toggle(btnStart, btnPause);
+        myCounter = setInterval(reloj.counter, 1000);
+    };
+
+    reloj.btnPa = () => {
+        reloj.toggle(btnPause, btnResume);
+        clearInterval(myCounter);
+    };
+
+    reloj.btnRe = () => {
+        reloj.toggle(btnResume, btnPause);
+        myCounter = setInterval(reloj.counter, 1000);
+    };
+
+    btnStart.on("click", reloj.btnSt);
+    btnPause.on("click", reloj.btnPa);
+    btnResume.on("click", reloj.btnRe);
+    return reloj;
 }
-
-
-//let sec = 0;
-//let min = 0;
-//const fun = () => {
-//    m = "";
-//    if (min < 10) {
-//        m = "0";
-//    }
-//    s = "";
-//    if (sec < 10) {
-//        s = "0";
-//    }
-//    timer.innerHTML = `${m}${min}:${s}${sec}`;
-//    sec = sec + 1;
-//    if (sec > 59) {
-//        sec = 0;
-//        min = min + 1;
-//    }
-//    if (min > 59) {
-//        min = 0;
-//    }
-//}
